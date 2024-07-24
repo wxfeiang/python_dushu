08979e3999adc2ae4ee1e96a0176af361b38e7bb
wxfeiang <wxfeiang@qq.com>
2024-07-24 23:03:50 +0800

    feat: loader

--- dushu/items.py
+++ dushu/items.py
@@ -1,21 +1,23 @@
-# Define here the models for your scraped items
-#
-# See documentation in:
-# https://docs.scrapy.org/en/latest/topics/items.html
 
 import scrapy
-from scrapy.loader.processors import MapCompose
+from scrapy.loader import ItemLoader
+from scrapy.loader.processors import MapCompose, TakeFirst
 
 
 # 还可以对里面的每一个值 处理
 
-def add_space(value):
-    return value + '----当前------》 '
+def add_jobbole(value):
+    return value + '自定义后缀------'
+
+
+class DushuItemLoader(ItemLoader):
+    # 自定义itemloader
+    default_output_processor = TakeFirst()
 
 class DushuItem(scrapy.Item):
     bookname = scrapy.Field()
     author = scrapy.Field(
-        input_processor = MapCompose(add_space)
+        input_processor = MapCompose(add_jobbole)
     )
     imgUrl = scrapy.Field()
     detailsUrl = scrapy.Field()
--- dushu/spiders/dushuList.py
+++ dushu/spiders/dushuList.py
@@ -1,7 +1,8 @@
 import scrapy
 from scrapy import Request
 
-from dushu.items import DushuItem
+from dushu.items import DushuItem, DushuItemLoader
+
 
 class DushulistSpider(scrapy.Spider):
     name = "dushuList"
@@ -16,22 +17,29 @@ class DushulistSpider(scrapy.Spider):
         #
         # print(bookList)
         for book in bookList:
-            item= DushuItem()
-            item['bookname'] = book.xpath('./div[@class="bookname"]/a/text()').extract_first()
-            item['author'] = book.xpath('./div[@class="bookauthor"]/text()').extract_first()
+            item_loader = DushuItemLoader(item=DushuItem(), response=response)
+            item_loader.add_value('bookname', book.xpath('./div[@class="bookname"]/a/text()').extract_first())
+            item_loader.add_value('author' , book.xpath('./div[@class="bookauthor"]/text()').extract_first())
             # FIX:图片下载需要 lsit 类型
-            item['imgUrl'] = [book.xpath('./div[@class="img152"]/a/img/@data-original').extract_first()]
-            item['detailsUrl'] = book.xpath('./div[@class="img152"]/a/@href').extract_first()
+            item_loader.add_value('imgUrl' , [book.xpath('./div[@class="img152"]/a/img/@data-original').extract_first()])
+            detailsUrl =  book.xpath('./div[@class="img152"]/a/@href').extract_first()
+            item_loader.add_value('detailsUrl' ,detailsUrl)
             # 需要根据链接打开在提取
-            yield scrapy.Request(url="https://www.dushu.com"+item["detailsUrl"], callback=self.details ,meta={"item":item})
+
+            yield scrapy.Request(
+                url="https://www.dushu.com" + detailsUrl,
+                callback=self.details ,
+                meta={"item": item_loader}
+            )
 
 
 
     def details(self,response):
-        item = response.meta["item"]
+        item_loader = response.meta["item"]
+
         summary = response.xpath('//div[@class="book-summary"][1]//div[@class="text txtsummary"]/text()').extract_first()
-        item['summary'] = summary
-        yield item
+        item_loader.add_value('summary', summary)
+        yield item_loader
 
 
 
